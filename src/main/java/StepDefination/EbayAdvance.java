32c67bdd7c47b686b3942335ebf64b9b757668d8
x295094 <deepak.manjunatha01@gmail.com>
2026-03-19 19:15:49 +0530

    Finished 1 testcase of advanced page

--- src/main/java/StepDefination/EbayAdvance.java
+++ src/main/java/StepDefination/EbayAdvance.java
@@ -1,27 +1,41 @@
 package StepDefination;
 
+import Managers.PageManager;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.testng.Assert;
 
 public class EbayAdvance {
+    PageManager pages = new PageManager();
 
     @Given("I am Ebay Advanced Search Page")
     public void i_am_ebay_advanced_search_page() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        pages.getHomePage().getEbayHomePage();
+        pages.getHomePage().isDisplayed(pages.getHomePage().ebayTitle);
+        Assert.assertEquals(pages.getHomePage().getText(pages.getHomePage().ebayTitle).toLowerCase(),"ebay home");
+
+        pages.getHomePage().isDisplayed(pages.getHomePage().advancedButton);
+        Assert.assertEquals(pages.getHomePage().getText(pages.getHomePage().advancedButton).toLowerCase(),"advanced");
+
+        pages.getAdvancedSearchPage().click(pages.getHomePage().advancedButton);
+        pages.getHomePage().isDisplayed(pages.getHomePage().ebayTitle);
+        pages.getAdvancedSearchPage().isDisplayed(pages.getAdvancedSearchPage().advancedSearchTitle);
     }
 
     @When("I click on Ebay Logo")
     public void i_click_on_ebay_logo() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        pages.getAdvancedSearchPage().isDisplayed(pages.getAdvancedSearchPage().ebayTitlelist);
+        pages.getHomePage().click(pages.getAdvancedSearchPage().ebayTitlelist);
     }
     @Then("" +
             "I am navigated to Ebay Home Page")
     public void i_am_navigated_to_ebay_home_page() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        pages.getHomePage().isDisplayed(pages.getHomePage().ebayTitle);
+        Assert.assertEquals(pages.getHomePage().getText(pages.getHomePage().ebayTitle).toLowerCase(),"ebay home");
     }
 
     @When("I advanced search an item")
