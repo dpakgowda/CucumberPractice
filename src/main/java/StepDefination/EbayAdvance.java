cc9904ab9410ee8f414ce0b46c1fca8fab06b4c5
x295094 <deepak.manjunatha01@gmail.com>
2026-03-20 21:39:53 +0530

    completed

--- src/main/java/StepDefination/EbayAdvance.java
+++ src/main/java/StepDefination/EbayAdvance.java
@@ -6,6 +6,9 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.testng.Assert;
 
+import java.util.List;
+import java.util.Map;
+
 public class EbayAdvance {
     PageManager pages = new PageManager();
 
@@ -47,6 +50,26 @@ public class EbayAdvance {
         // Double, Byte, Short, Long, BigInteger or BigDecimal.
         //
         // For other transformations you can register a DataTableType.
-        throw new io.cucumber.java.PendingException();
+        List<List<String>> rows = dataTable.asLists(String.class);
+
+        pages.getAdvancedSearchPage().isDisplayed(pages.getAdvancedSearchPage().searchKeyword);
+        //pages.getAdvancedSearchPage().click(pages.getAdvancedSearchPage().searchKeyword);
+        pages.getAdvancedSearchPage().jsClick(pages.getAdvancedSearchPage().searchKeyword);
+        pages.getAdvancedSearchPage().setValueUsingJS(pages.getAdvancedSearchPage().searchKeyword, rows.get(1).get(0));
+
+        pages.getAdvancedSearchPage().click(pages.getAdvancedSearchPage().exclude);
+        pages.getAdvancedSearchPage().sendKeys(pages.getAdvancedSearchPage().exclude, rows.get(1).get(1));
+
+        pages.getAdvancedSearchPage().scrollToElement(pages.getAdvancedSearchPage().minimum);
+        pages.getAdvancedSearchPage().click(pages.getAdvancedSearchPage().minimum);
+        pages.getAdvancedSearchPage().sendKeys(pages.getAdvancedSearchPage().minimum, rows.get(1).get(2));
+
+        pages.getAdvancedSearchPage().click(pages.getAdvancedSearchPage().maximum);
+        pages.getAdvancedSearchPage().sendKeys(pages.getAdvancedSearchPage().maximum, rows.get(1).get(3));
+        pages.getAdvancedSearchPage().scrollToElement(pages.getAdvancedSearchPage().Search);
+        pages.getAdvancedSearchPage().click(pages.getAdvancedSearchPage().Search);
+
+        pages.getHomePage().waitForVisibility(pages.getHomePage().Result);
+        Assert.assertTrue(pages.getHomePage().Result.getText().contains(rows.get(1).get(1)));
     }
 }
--- src/test/java/Runner/MainRunner2.java
+++ src/test/java/Runner/MainRunner2.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         monochrome = true,
         dryRun = false,
         plugin = {"pretty","html:target/cucumber","json:target/cucumber.json"},
-        tags = "@P24"
+        tags = ""
 )
 
 public class MainRunner2  {
